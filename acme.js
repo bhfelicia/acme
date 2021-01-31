f70bfda3fc8afe78daf250c67a4bfc37fe6aed35
Felicia Heiney <bhfelicia@gmail.com>
2021-01-30 22:20:58 -0500

    server, flexbox, and html work

---
+++ acme.js
@@ -0,0 +1,10 @@
+const slots = ["first", "second", "third"];
+
+const users = [
+  { id: 1, name: "joe", slot: "first" },
+  { id: 2, name: "carrie", slot: "second" },
+  { id: 3, name: "girly", slot: "third" },
+  { id: 4, name: "sushi", slot: "third", selected: true },
+];
+
+module.exports = { slots, users };
